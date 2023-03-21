3dbda94df8f5a9d085e0dfe8143c9925053329b1
peguin237 <kieth540@gmail.com>
2023-03-21 07:25:03 +0700

    Add project files.

---
+++ baitapltw/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using baitapltw.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baitapltw.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        private readonly ApplicationDbContext dbContext = new ApplicationDbContext();
+        public ActionResult Details(int id)
+        {
+            var product = dbContext.Products.FirstOrDefault(x => x.Id == id);
+            return View(product);
+        }
+        public ActionResult Create()
+        {
+            var ds = dbContext.Categories.ToList();
+            ViewBag.categories = ds;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SaveProduct(Product product, HttpPostedFileBase FeatureImage)
+        {
+            if(!ModelState.IsValid)
+            {
+                var ds = dbContext.Categories.ToList();
+                ViewBag.categories = ds;
+                return View("Create",product);
+            }
+            string path = Path.Combine(Server.MapPath("~/Content/NoiThat/images"), Path.GetFileName(FeatureImage.FileName));
+            FeatureImage.SaveAs(path);
+            product.FeatureImage = "Content/NoiThat/images/" + Path.GetFileName(FeatureImage.FileName);
+            dbContext.Products.Add(product);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ baitapltw/Models/Product.cs
@@ -0,0 +1,31 @@
+namespace baitapltw.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Product")]
+    public partial class Product
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        //Model DataAnnotations
+        [Required(ErrorMessage = "Vui long nhap tieu de")]
+        public string Title { get; set; }
+
+        public decimal? Price { get; set; }
+
+        [StringLength(200)]
+        public string FeatureImage { get; set; }
+
+        public int? ProductCateId { get; set; }
+
+        [StringLength(500)]
+        public string Des { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
